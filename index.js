74b4faf52e2c81cf98cbbbdc3371e1664385fc96
Shubham Mangal <shubham.mangal15@gmail.com>
2017-11-26 18:00:29 +0530

    added server details

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
