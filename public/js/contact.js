8f889652f113d03a87ff2014ac1457995262dee1
Shubham Mangal <shubham.mangal15@gmail.com>
2017-12-02 17:33:03 +0530

    contact form

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,7 @@
 
 
 var $form = $('form#contact-form'),
-url = 'https://script.google.com/macros/u/1/s/AKfycbyB4_xa1pfu17YEbGT76howphOHKIdVYiyiM71Ng-fpKcXypD15/exec'
+url = 'https://script.google.com/macros/s/AKfycbyB4_xa1pfu17YEbGT76howphOHKIdVYiyiM71Ng-fpKcXypD15/exec'
 
 $('#submit-form').on('click', function(e) {
 e.preventDefault();
@@ -9,7 +9,6 @@ e.preventDefault();
 $.ajax({
     url: url,
     method: "GET",
-    dataType: "json",
     data: $form.serializeObject(),					
     success: function(data) {
         console.log('success');
