c9a91e4aa58614b36a02821e027ad584286e5a40
Shubham Mangal <shubham.mangal15@gmail.com>
2018-01-13 02:40:40 +0530

    minor change

--- routes/sheet.js
+++ routes/sheet.js
@@ -8,9 +8,30 @@ var Payment = require('../schema/payment');
 var fs = require('fs');
 var PDFDocument = require('pdfkit');
 
-/* router.get('/', Verify.verifyOrdinaryUser, function (req, res, next) {
-  
-    Utils.saveSheet();
-  }); */
+ router.get('/', Verify.verifyOrdinaryUser, function (req, res, next) {
+  Payment.find({ 'event.clubName' : 'Quizzing'}, function(err, payment) {
+            
+            
+    if (err)
+        return done(err);
+   
+    if (payment){
+     
+     
+      for(var i=0; i<payment.length; i++){
+       
+        
+        Utils.saveSheet(payment[i]);
+          
+          //   
+      
+      }
+   
+      
+    }
+});
+ 
+    
+  }); 
   
 module.exports = router;
